e16dd01d7a2abc3b99a077e56aa31be57ee1dd07
Dylan Thinnes <dylan.thinnes@protonmail.com>
2024-11-02 20:03:28 +0000

    Implement gradient

--- rgb-gradient/rgb-gradient.v
+++ rgb-gradient/rgb-gradient.v
@@ -3,57 +3,90 @@
 `include "../include/led-pwm.v"
 
 module top (
-        input         CLK,
-        input         BTN_N,
-        output [15:0] LED_PANEL);
-
-        led_main #(
-            .FRAME_BITS(13),
-            .DELAY(1)
-        ) main (
-            .CLK(CLK),
-            .resetn_btn(BTN_N),
-            .LED_PANEL(LED_PANEL));
-
+    input         CLK,
+    input         BTN_N,
+    output [15:0] LED_PANEL
+);
+    led_main #(
+        .FRAME_BITS(13),
+        .DELAY(1)
+    ) main (
+        .CLK(CLK),
+        .resetn_btn(BTN_N),
+        .LED_PANEL(LED_PANEL)
+    );
 endmodule
 
 module painter24(
-        input        clk,
-        input        reset,
-        input  [12:0] frame,
-        input  [7:0] subframe,
-        input  [5:0] x,
-        input  [5:0] y,
-        output [23:0] rgb24);
-
+    input         clk,
+    input         reset,
+    input  [12:0] frame,
+    input  [7:0]  subframe,
+    input  [5:0]  x,
+    input  [5:0]  y,
+    output [23:0] rgb24
+);
     wire [7:0] red, green, blue;
 
-    reg [24:0] divide;
-    reg [5:0] limit_x_div, limit_y_div;
-    reg [5:0] limit_x_sub, limit_y_sub;
+    wire [5:0] truex, truey;
+    assign truex = x - 6'b000001;
+    assign truey = x == 0 ? {y[5], y[4:0] - 5'b00001} : y;
 
-    reg [12:0] last_frame;
+    wire [5:0] distx, disty, dist;
+    wire [5:0] dist;
+    assign distx = truex < 32 ? 32 - truex : truex - 32;
+    assign disty = truey < 32 ? 32 - truey : truey - 32;
+    assign dist = distx + disty;
 
-    always @(posedge clk) begin
-        if (divide == 240000) begin
-            divide <= 0;
-            limit_x_div <= limit_x_div + 1;
-            limit_y_div <= limit_y_div + (limit_x_div == 63);
-            //active <= !active;
-        end else begin
-            divide <= divide + 1;
-        end
+    wire [7:0] scalar;
+    wire [15:0] scaled_dist;
+    assign scaled_dist = { {8{scalar[7]}}, scalar[7:0] } * {10'd0, dist};
+    assign scalar = 100;
 
-        if (last_frame != frame) begin
-            last_frame <= frame;
-            limit_x_sub <= limit_x_sub + 1;
-            limit_y_sub <= limit_y_sub + (limit_x_sub == 63);
-        end
-    end
+    hue2rgb #(.POWER(1)) hsv_driver(
+        .hue(frame[7:0] + scaled_dist[15:8]),
+        .red(red),
+        .green(green),
+        .blue(blue)
+    );
 
-    assign red = y == limit_y_div && x == limit_x_div ? 200 : 0;
-    assign green = y == limit_y_sub && x == limit_x_sub ? 200 : 0;
-    assign blue = 0; // x == limit_x_sub ? 200 : 0;
     assign rgb24 = {red, green, blue};
 
 endmodule
+
+module hue2rgb#(
+    parameter [2:0] POWER = 3'd6 // from 1 to 6
+)(
+    input  [7:0] hue,
+    output [7:0] red,
+    output [7:0] green,
+    output [7:0] blue);
+
+    hsv_channel #(.POWER(POWER)) red_driver(
+        .hue(hue + 85),
+        .value(red)
+    );
+    hsv_channel #(.POWER(POWER)) green_driver(
+        .hue(hue),
+        .value(green)
+    );
+    hsv_channel #(.POWER(POWER)) blue_driver(
+        .hue(hue + 170),
+        .value(blue)
+    );
+endmodule
+
+module hsv_channel#(
+        parameter [2:0] POWER = 3'd6 // from 1 to 6
+)(
+        input  [7:0] hue,
+        output [7:0] value);
+
+    assign value =
+                hue < 43  ? hue * POWER
+              : hue < 128 ? 43 * POWER
+              : hue < 170 ? (170 - hue) * POWER
+                          : 0;
+
+endmodule
+
